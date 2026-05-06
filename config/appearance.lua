7272e8a91631548f672e5b96cc3ffb1b40f2049d
andyalv <andy.red02@gmail.com>
2026-05-06 09:05:41 -0600

    Refactor appearance handling to dynamically select theme based on GUI appearance

--- config/appearance.lua
+++ config/appearance.lua
@@ -2,10 +2,18 @@ local wezterm = require("wezterm")
 
 local module = {}
 
+function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return 'Dark'
+end
+
 function module.apply_to_config(config)
     -- Rose Pine theme
     local rose_pine = wezterm.plugin.require('https://github.com/neapsix/wezterm')
-    local theme = rose_pine.moon -- alternatives: main, moon, dawn
+    local appearance = get_appearance()
+    local theme = appearance:find("Dark") and rose_pine.main or rose_pine.dawn
 
     -- Apply theme colors and window frame
     config.colors = theme.colors()
