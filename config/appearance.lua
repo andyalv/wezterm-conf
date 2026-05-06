e71842824521c54cbb4605e27eab6572eb82cfea
andyalv <andy.red02@gmail.com>
2026-05-06 10:06:19 -0600

    Refactor appearance handling to simplify theme selection logic

--- config/appearance.lua
+++ config/appearance.lua
@@ -2,18 +2,18 @@ local wezterm = require("wezterm")
 
 local module = {}
 
-function get_appearance()
-    if wezterm.gui then
-        return wezterm.gui.get_appearance()
-    end
-    return 'Dark'
-end
-
 function module.apply_to_config(config)
     -- Rose Pine theme
     local rose_pine = wezterm.plugin.require('https://github.com/neapsix/wezterm')
-    local appearance = get_appearance()
-    local theme = appearance:find("Dark") and rose_pine.main or rose_pine.dawn
+
+    -- Appearance options
+    local appearance_themes =  {
+        Dark = rose_pine.main,
+        Light = rose_pine.dawn,
+    }
+
+    -- Get the appearance setting from environment variable or default to "Dark"
+    local theme = appearance_themes[appearance] or appearance_themes.Dark
 
     -- Apply theme colors and window frame
     config.colors = theme.colors()
